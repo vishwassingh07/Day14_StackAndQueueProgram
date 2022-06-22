d4e249d25034ee4b70e25650e550e95cb6642ede
Vishwas <baghel.vishwas0@gmail.com>
2022-06-22 14:31:05 +0530

    [Vishwas]add: Stack And Queue Program Added

---
+++ StackAndQueueProgram/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace StackAndQueueProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Stack and Queue program \n ---------------------------------------");
+        }
+    }
+}
